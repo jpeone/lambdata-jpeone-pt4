b4c02460f9ef61b21546408886f15df4a2c67fc5
jpeone <jesse.gene.peone@gmail.com>
2020-03-13 19:20:12 -0700

    making lambdata.my_mod OOO and pep8 stylistic

--- lambdata/my_mod.py
+++ lambdata/my_mod.py
@@ -4,52 +4,60 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-def train_val_test_split(X, y, test_size = .3, val_size = .1, train_size = .6, 
-random_state = None, shuffle = True, stratify = None):
-    '''
-    Fragile, no exception handling for the sum of test_size, val_size, train_size.
-    We're all adults here.  Make the sum of them equal = 1
-    '''
+class Helper:
 
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, train_size = train_size,
-        random_state = random_state, shuffle = shuffle, stratify = stratify)
+    def __init__(self):
+        #nothing to go in here yet
+        pass
 
-    split = 1 / ((test_size + val_size) * 10)
+    def train_val_test_split(self, X, y, test_size = .3, val_size = .1, train_size = .6, 
+    random_state = None, shuffle = True, stratify = None):
+        '''
+        Fragile, no exception handling for the sum of test_size, val_size, train_size.
+        We're all adults here.  Make the sum of them equal = 1
+        '''
 
-    split = split * val_size * 10
+        X_train, X_temp, y_train, y_temp = train_test_split(X, y, train_size = train_size,
+            random_state = random_state, shuffle = shuffle, stratify = stratify)
 
-    if stratify is not None:
-        strat = y_temp
-    else:
-        strat = None
-    
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, train_size = split,
-        random_state = random_state, shuffle = shuffle, stratify = strat)
+        split = 1 / ((test_size + val_size) * 10)
 
-    return(X_train, X_val, X_test, y_train, y_val, y_test)
+        split = split * val_size * 10
 
-def tocol(df, colist, column):
-    '''
-    turn a list into a column
-    params:
-        df - dataframe to add column to
-        colist - list to be turned into a column
-        column - string of the column name
-    '''
-    df[column] = pd.Series(colist)
-    return df
+        if stratify is not None:
+            strat = y_temp
+        else:
+            strat = None
+        
+        X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, train_size = split,
+            random_state = random_state, shuffle = shuffle, stratify = strat)
+
+        return(X_train, X_val, X_test, y_train, y_val, y_test)
+
+    def tocol(self, df, colist, column):
+        '''
+        turn a list into a column
+        params:
+            df - dataframe to add column to
+            colist - list to be turned into a column
+            column - string of the column name
+        '''
+        df[column] = pd.Series(colist)
+        return df
 
 
-def enlarge(n):
-    return n * 100
+    def enlarge(self, n):
+        return n * 100
 
 
 def main():
     print("junk")
     print('global_scope')
 
+    helper = Helper()
+
     y = float(input('please input a number to enlarge:'))
-    print(enlarge(y))
+    print(helper.enlarge(y))
 
 if __name__ == "__main__":
     main()
--- lambdata/my_script.py
+++ lambdata/my_script.py
@@ -1,7 +1,10 @@
 #lambdata/my_script.py
+#this is shaping up to be a test file
 
 import pandas as pd
-from lambdata.my_mod import enlarge, train_val_test_split, tocol
+from lambdata.my_mod import Helper
+
+helper = Helper()
 
 print('Happy Tuesday Night.')
 
@@ -9,7 +12,7 @@ df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
 print(df.head())
 
 x = 5
-print('Enlarge', x, 'to', enlarge(x))
+print('Enlarge', x, 'to', helper.enlarge(x))
 
 print('Testing train_val_test_split')
 
@@ -19,12 +22,12 @@ df2 = pd.DataFrame({'a': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 
 df2 = pd.concat([df2, df2])
 
-X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(df2.drop(columns = 'c'), df2['c'], stratify = df2['c'])
+X_train, X_val, X_test, y_train, y_val, y_test = helper.train_val_test_split(df2.drop(columns = 'c'), df2['c'], stratify = df2['c'])
 
 print(df2.shape)
 print(X_train.shape, X_val.shape, X_test.shape, y_train.shape, y_val.shape, y_test.shape)
 print('The train, val, test split has the right shape:')
 print(X_train.shape[0] + X_val.shape[0] + X_test.shape[0] == df2.shape[0])
 
-df3 = tocol(df, [7, 8, 9], 'z')
+df3 = helper.tocol(df, [7, 8, 9], 'z')
 print(df3)

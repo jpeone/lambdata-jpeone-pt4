d2217a75b77912113df9f378063a6f7b85ef3e82
jpeone <jesse.gene.peone@gmail.com>
2020-03-16 20:23:41 -0700

    basic inheritance examples

--- lambdata/assignment.py
+++ lambdata/assignment.py
@@ -4,39 +4,24 @@ import pandas
 
 def main():
     print('-------------------------------')
-    df = pandas.DataFrame({"abbrev": ['ca', 'ct', 'co', 'tx', 'dc']})
+    df = CustomFrame({"abbrev": ['ca', 'ct', 'co', 'tx', 'dc']})
 
-    # new_df = add_state_name(df)
-    # print(new_df.head())
-    processor = DataProcessor(df)
+    print(df.head())
 
-    print(processor.df.head())
-
-    processor.add_state_name()
-    print(processor.df.head())
+    df.add_state_name()
+    print(df.head())
 
     print('-------------------------------')
-    df2 = pandas.DataFrame({'abbrev': ['oh', 'mi', 'id', 'wa', 'wi']})
-    processor2 = DataProcessor(df2)
-    
+    df2 = CustomFrame({'abbrev': ['oh', 'mi', 'id', 'wa', 'wi']})
 
-    print(processor2.df.head())
+    print(df2.head())
     
-    processor2.add_state_name()
-    print(processor2.df.head())
+    df2.add_state_name()
+    print(df2.head())
 
     print('-------------------------------')
-#state abbreviation -? Ful Name and visa versa. 
 
-class DataProcessor():
-
-    def __init__(self, my_df):
-        '''
-        param:
-            my_df (pandas.DataFrame) containing a colum called 'abbrev'
-        '''
-        self.df = my_df.copy()
-
+class CustomFrame(pandas.DataFrame):
 
     def add_state_name(self):
         '''
@@ -56,7 +41,7 @@ class DataProcessor():
             'wi': 'Wisconsin'
         }
 
-        self.df['name'] = self.df['abbrev'].map(names_map)
+        self['name'] = self['abbrev'].map(names_map)
 
 
 if __name__ == '__main__':

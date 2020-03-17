2c19e06efa8311b37ce0b8253109851e8cd4b7f9
jpeone <jesse.gene.peone@gmail.com>
2020-03-16 20:17:50 -0700

    basic oop example

--- lambdata/assignment.py
+++ lambdata/assignment.py
@@ -5,45 +5,58 @@ import pandas
 def main():
     print('-------------------------------')
     df = pandas.DataFrame({"abbrev": ['ca', 'ct', 'co', 'tx', 'dc']})
-    print(df.head())
 
-    new_df = add_state_name(df)
-    print(new_df.head())
+    # new_df = add_state_name(df)
+    # print(new_df.head())
+    processor = DataProcessor(df)
+
+    print(processor.df.head())
+
+    processor.add_state_name()
+    print(processor.df.head())
 
     print('-------------------------------')
     df2 = pandas.DataFrame({'abbrev': ['oh', 'mi', 'id', 'wa', 'wi']})
-    print(df2.head())
+    processor2 = DataProcessor(df2)
     
-    new_df2 = add_state_name(df2)
-    print(new_df2.head())
+
+    print(processor2.df.head())
+    
+    processor2.add_state_name()
+    print(processor2.df.head())
 
     print('-------------------------------')
 #state abbreviation -? Ful Name and visa versa. 
 
-def add_state_name(my_df):
-    '''
-    param:
-        my_df (pandas.DataFrame) containing a colum called 'abbrev'
-    '''
-    names_map = {
-        'ca': 'California',
-        'ct': 'Connecticut',
-        'co': 'Colorado',
-        'tx': 'Texas',
-        'dc': 'Washington DC',
-        'oh': 'Ohio',
-        'mi': 'Michigan',
-        'id': 'Idaho',
-        'wa': 'Washington',
-        'wi': 'Wisconsin'
-    }
+class DataProcessor():
 
-    new_df = my_df.copy()
-
-    new_df['name'] = new_df['abbrev'].map(names_map)
-    return new_df
+    def __init__(self, my_df):
+        '''
+        param:
+            my_df (pandas.DataFrame) containing a colum called 'abbrev'
+        '''
+        self.df = my_df.copy()
 
 
+    def add_state_name(self):
+        '''
+        param:
+            my_df (pandas.DataFrame) containing a colum called 'abbrev'
+        '''
+        names_map = {
+            'ca': 'California',
+            'ct': 'Connecticut',
+            'co': 'Colorado',
+            'tx': 'Texas',
+            'dc': 'Washington DC',
+            'oh': 'Ohio',
+            'mi': 'Michigan',
+            'id': 'Idaho',
+            'wa': 'Washington',
+            'wi': 'Wisconsin'
+        }
+
+        self.df['name'] = self.df['abbrev'].map(names_map)
 
 
 if __name__ == '__main__':
